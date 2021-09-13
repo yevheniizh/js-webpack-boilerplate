647eabef7816a52ce88c465ed9ba291dd5d1dc60
Yevhenii Zhyrov <zh.zhyrov@gmail.com>
2021-09-13 19:35:13 +0300

    Init app

---
+++ README.md
@@ -0,0 +1,13 @@
+## Running on localhost
+
+First install dependencies:
+
+```sh
+npm install
+```
+
+## Running
+
+```sh
+npm run start
+```
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const webpack = require("webpack");
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+const config = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.svg$/,
+        use: "file-loader",
+      },
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: "public/index.html" }],
+    }),
+    new HtmlWebpackPlugin({
+      filename: "public/index.html",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
+
+module.exports = config;
